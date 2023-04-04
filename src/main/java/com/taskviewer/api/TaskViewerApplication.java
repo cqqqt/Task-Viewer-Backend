80d1447f40d741c142837ff6930f4365a71793c3
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-04-04 16:37:53 +0300

    #4

---
+++ src/main/java/com/taskviewer/api/TaskViewerApplication.java
@@ -0,0 +1,15 @@
+package com.taskviewer.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Entry point.
+ */
+@SpringBootApplication
+public class TaskViewerApplication {
+
+  public static void main(final String... args) {
+    SpringApplication.run(TaskViewerApplication.class, args);
+  }
+}
