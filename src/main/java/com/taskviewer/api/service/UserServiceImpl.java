5e81cbb987b8d5ae9afc848c55c6c85419e852ab
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-04-10 18:22:08 +0300

    UserServiceImpl

--- src/main/java/com/taskviewer/api/service/UserServiceImpl.java
+++ src/main/java/com/taskviewer/api/service/UserServiceImpl.java
@@ -1,13 +1,13 @@
 package com.taskviewer.api.service;
 
 import com.taskviewer.api.model.User;
+import com.taskviewer.api.model.UserAlreadyExistsException;
 import com.taskviewer.api.model.UserNotFoundException;
 import com.taskviewer.api.model.Users;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Transactional(readOnly = true)
 @Component
@@ -19,8 +19,27 @@ public class UserServiceImpl implements UserService {
   @Transactional
   @Override
   public User with(final User user) {
-    this.users.add(user);
-    return this.byUsername(user.username());
+    final String email = user.email();
+    if (this.users.byEmail(email).isPresent()) {
+      throw new UserAlreadyExistsException(
+        "User with email %s already exists"
+          .formatted(
+            email
+          )
+      );
+    }
+    final String username = user.username();
+    if (this.users.byUsername(username).isPresent()) {
+      throw new UserAlreadyExistsException(
+        "User with username %s already exists"
+          .formatted(
+            username
+          )
+      );
+    } else {
+      this.users.add(user);
+      return this.byUsername(user.username());
+    }
   }
 
   @Transactional
