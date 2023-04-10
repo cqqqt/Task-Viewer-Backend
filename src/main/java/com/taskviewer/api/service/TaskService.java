562f013016b617efaaa8962ddcf994df4d36cf5d
cichan <6684177pt@gmail.com>
2023-04-10 20:56:10 +0300

    solved merge conflict

--- src/main/java/com/taskviewer/api/service/TaskService.java
+++ src/main/java/com/taskviewer/api/service/TaskService.java
@@ -2,7 +2,26 @@ package com.taskviewer.api.service;
 
 import com.taskviewer.api.model.Task;
 
+import java.util.List;
+
 public interface TaskService {
 
+  Task byId(Long id);
+
+  List<Task> byUsername(String username);
+
+  List<Task> byEmail(String email);
+
+  List<Task> withPriority(int priority);
+
+  List<Task> withStatus(String status);
+
+  List<Task> all();
+
+  Task add(Task task);
+
+  Task update(Task task);
+
   Task assign(Long id, Long user);
+
 }
--- src/main/java/com/taskviewer/api/web/rq/RqTask.java
+++ src/main/java/com/taskviewer/api/web/rq/RqTask.java
@@ -1,6 +1,13 @@
 package com.taskviewer.api.web.rq;
 
-public record RqTask(
+import com.taskviewer.api.model.Status;
+import com.taskviewer.api.model.TimeEstimate;
 
+public record RqTask(
+		String username,
+		String title,
+		String about,
+		Status status,
+		TimeEstimate timeEstimate
 ) {
 }
