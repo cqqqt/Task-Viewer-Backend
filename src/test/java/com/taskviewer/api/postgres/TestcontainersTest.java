b0f23e5885ec42138bd0806c3cd86ef49051af26
cpiskunou solvd <cpiskunou@solvd.com>
2023-04-10 15:39:07 +0300

    remove integration test

--- src/test/java/com/taskviewer/api/postgres/TestcontainersTest.java
+++ src/test/java/com/taskviewer/api/postgres/TestcontainersTest.java
@@ -1,10 +1,16 @@
 package com.taskviewer.api.postgres;
 
+import com.taskviewer.api.TaskViewerApplication;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+@SpringBootTest(classes = TaskViewerApplication.class)
+@ExtendWith(SpringExtension.class)
 @Sql({"schema.sql", "test-data.sql"})
 public abstract class TestcontainersTest {
 
