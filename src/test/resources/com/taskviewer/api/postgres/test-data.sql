252bbd454f848c20c3ab52b501e9f4d37837ef7b
cpiskunou solvd <cpiskunou@solvd.com>
2023-04-11 20:11:11 +0300

    remove unnecessary connecting with failed trying to write integrations tests

--- src/test/resources/com/taskviewer/api/postgres/test-data.sql
+++
@@ -1,5 +0,0 @@
-insert into login(username, firstname, lastname, password, role, email)
-            values('alex', 'Aliaksei', 'Bialiauski', encode('alex', 'base64'), 'ADMIN', 'alex@gmail.com');
-
-insert into task(title, about, assigne, priority, status, estimate, tracked)
-            values ('Integration tests', 'Implements integration test for PgTasks', 1, 1, 'HIGH', now(), now());
