38409d94fe1c554bbca64aeb86af61e256ff5f06
Aliaksei Bialiauski <Hialexbel@gmail.com>
2023-04-13 17:25:31 +0300

    tracked parse

--- src/main/java/com/taskviewer/api/postgres/ViewTask.java
+++ src/main/java/com/taskviewer/api/postgres/ViewTask.java
@@ -3,6 +3,7 @@ package com.taskviewer.api.postgres;
 import com.taskviewer.api.model.Status;
 import com.taskviewer.api.model.Task;
 import com.taskviewer.api.model.TimeEstimate;
+import java.time.LocalDateTime;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
@@ -13,23 +14,31 @@ import java.sql.SQLException;
 @Component
 public class ViewTask implements RowMapper<Task> {
 
-	@Override
-	public Task mapRow(@NotNull ResultSet rs, int rowNum) throws SQLException {
-		return PgTask.builder()
-				.id( rs.getLong("task_id") )
-				.username( rs.getString("username") )
-				.title( rs.getString( "title") )
-				.about( rs.getString( "about") )
-				.status(new Status.Simple(
-						rs.getString("status"),
-						rs.getInt("priority") )
-				)
-				.time(new TimeEstimate.InMinutes(
-						rs.getTimestamp("estimate").toLocalDateTime(),
-						rs.getTimestamp("tracked").toLocalDateTime() )
-				)
-				.created( rs.getTimestamp("task_created").toLocalDateTime() )
-				.build();
-	}
+  @Override
+  public Task mapRow(@NotNull final ResultSet rs, final int rows)
+    throws SQLException {
+    final LocalDateTime tracked;
+    if (rs.getTimestamp("tracked") != null) {
+      tracked = rs.getTimestamp("tracked").toLocalDateTime();
+    } else {
+      tracked = null;
+    }
+    return PgTask.builder()
+      .id(rs.getLong("task_id"))
+      .username(rs.getString("username"))
+      .title(rs.getString("title"))
+      .about(rs.getString("about"))
+      .status(new Status.Simple(
+        rs.getString("status"),
+        rs.getInt("priority"))
+      )
+      .time(new TimeEstimate.InMinutes(
+          rs.getTimestamp("estimate").toLocalDateTime(),
+          tracked
+        )
+      )
+      .created(rs.getTimestamp("task_created").toLocalDateTime())
+      .build();
+  }
 
 }
